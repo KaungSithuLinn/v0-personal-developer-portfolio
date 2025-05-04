8b5dd5818287e61fb75100487eff32e38b326740
KaungSithuLinn <71718819+KaungSithuLinn@users.noreply.github.com>
2025-05-04 22:51:41 +0800

    chore: add dynamic export to page component for improved rendering

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = "force-dynamic"
+
 import Hero from "../components/Hero"
 import About from "../components/About"
 import Experience from "../components/Experience"
